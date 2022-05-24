a636a812a2e152bb0765bf6474b14cf3394303e1
Austin <austinhermesdev@gmail.com>
2022-05-24 16:53:49 -0400

    Updated with HLY integration

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ const ctx3 = new web3.eth.Contract(jsonInterface, '0xB3617363eDEc16cB0D30a5912Eb
 const ctx4 = new web3.eth.Contract(jsonInterface, '0x3074cf20ecd1cfe96b3ee43968d0c426f775171a'); // DAI Bank
 const ctx5 = new web3.eth.Contract(jsonInterface, '0x88Cc1D5E92aE19441583968EEc1cd03BEF47B5ED'); // HLY Bank
 
+const banks = {
+  "0x3074cf20ECD1Cfe96b3Ee43968d0c426f775171a": "dai",
+  "0xB3617363eDEc16cB0D30a5912Eb7A6B1D48e2875": "lumen",
+  "0xb684CAB219dE861a49b396Ae3BbB1fc8702286E3": "magic",
+  "0x3636421e71dCF0Bfcbb08FeeB62E0275ea5AcD61": "uni",
+  "0x88Cc1D5E92aE19441583968EEc1cd03BEF47B5ED": "hly",
+};
+
 let balances = {}, bytx = [];
 async function events(ctx) {
     let size = 1000;
@@ -43,15 +51,14 @@ async function events(ctx) {
                         const amount = parseInt(user.amount)/1e18;
                         balances[user.user] += amount;
                         console.log('\t', user.user, amount);
-
-                        bytx.push(`${e.transactionHash},${user.user},${amount}`)
-
+                        bytx.push(`${e.transactionHash},${user.user},${amount}`);
                     }
                 }
             });
     }
 
 }
+
 
 async function scanBlockchain(){
     await events(ctx1);
@@ -63,51 +70,82 @@ async function scanBlockchain(){
     console.log('\tscan completed and bytx.txt generated with all tx.')
 }
 
-async function generateBalance(){
-    console.log('loading bytx.txt...');
-    const bytx = fs.readFileSync('./bytx.txt', 'utf-8').split('\n');
-    let balancesArray = [];
-    for( let i in bytx ){
-        const id = bytx[i].split(',')[1];
-        if( balances[id] ) continue;
-        balances[id] = true;
-        balancesArray.push(id);
-    }
-    let txt = [];
-    const balancesTotal = balancesArray.length;
-    console.log('building balances... total '+balancesTotal);
-    for( let i = 0 ; i < balancesTotal; i ++ ){
-        const address = balancesArray[i];
-        const balance1 = await balance(address,ctx1);
-        const balance2 = await balance(address,ctx2);
-        const balance3 = await balance(address,ctx3);
-        const balance4 = await balance(address,ctx4);
-        const balance5 = await balance(address,ctx5);
-        const total = balance1 + balance2 + balance3 + balance4 + balance5;
-        const unlockedbank = balance1 + balance4 + balance3 + balance2; // Unlock before May 11th
-        const lockedbank = balance5; // Unlock after May 11th
-        const HRMS = total * RATIO;
-        const unlockedHRMS = unlockedbank * RATIO;
-        const airdropHRMS = lockedbank * RATIO;
-        const info = address+","+total+","+ unlockedbank +","+ lockedbank +","+RATIO+","+HRMS+","+unlockedHRMS+","+airdropHRMS;
-        // const info = address+","+total+","+lockedbank+","+RATIO+","+HRMS+","+airdropHRMS;
-        txt.push( info );
-        console.log(i+' of '+balancesTotal+') '+info);
-    }
-    console.log('writing all_addresses.txt');
-    fs.writeFileSync('./all_addresses.txt', txt.join('\n'));
 
-    let txtFiltered = [];
-    for (let i = 0; i < txt.length; i++) {
-        if (txt[i].split(',').at(3) > 0) {
-            txtFiltered.push(txt[i]);
-        }
+async function generateNewBalance() {
+  let balancesArray = [];
+  let result = [];
+  let txt = [];
+
+  // 1st we read the transactions file
+  console.log(`loading bytx.txt...`);
+  const bytx = fs.readFileSync("./bytx.txt", "utf-8").split("\n");
+
+  // We create an array of objects with all the lines from bytx file
+  for (let i in bytx) {
+    const line = {
+      tx: bytx[i].split(",")[0],
+      account: bytx[i].split(",")[1],
+      amount: bytx[i].split(",")[2],
+    };
+    balancesArray.push(line);
+  }
+
+  //Here we group all the information by account and classify them by bank
+  result = await balancesArray.reduce(async function (previousValue, value) {
+    //As this loop has async function we need to await from the previous iteraction
+    let array = await previousValue;
+
+    //Check if we have this account
+    if (!array[value.account]) {
+      array[value.account] = {
+        account: value.account,
+        whitelisted_PLTS: 0,
+        whitelisted_pHRMS: 0
+      };
     }
 
-    console.log('writing affected_addresses.txt');
-    fs.writeFileSync('./affected_addresses.txt', txtFiltered.join('\n'));
-    console.log('done.');
+
+    //Information of the transaction
+    let bank = await web3.eth.getTransaction(value.tx);
+    //Classification by bank
+    if (banks[bank.to]) {
+      if (bank.blockNumber <= 23670112) {
+        array[value.account].whitelisted_PLTS += +(value.amount);
+        array[value.account].whitelisted_pHRMS += +(value.amount * RATIO);
+        console.log(`${banks[bank.to]} ETH: ${array[value.account].whitelisted_PLTS}`);
+      } 
+    };
+    return array;
+  }, Promise.resolve([]));
+
+
+  //Write the whitelist
+  console.log("writing accounts_whitelist.txt");
+  txt.push("account,whitelisted_PLTS,whitelisted_pHRMS");
+  for (let i in result) {
+    txt.push(
+      `${result[i].account},${result[i].whitelisted_PLTS},${result[i].whitelisted_pHRMS}`
+    );
+  }
+  fs.writeFileSync("./accounts_whitelist.txt", txt.join("\n"));
+  console.log("done");
+  return true;
 }
+
+
+async function formatWhitelist() {
+  let formatted = [];
+  // 1st we read the transactions file
+  console.log(`loading accounts_whitelist.txt...`);
+  const whitelist = fs.readFileSync("./accounts_whitelist.txt", "utf-8").split("\n");
+  console.log("writing accounts_PLTS.csv");
+  for (let i in whitelist) {
+    formatted.push(`${whitelist[i].split(",")[0]},${whitelist[i].split(",")[1]}`);
+    fs.writeFileSync("./accounts_PLTS.csv", formatted.join("\n"));
+  }
+}
+
+
 async function balance(user, ctx){
     const info4 = await ctx.methods.userInfo(user).call();
     return parseFloat( web3.utils.fromWei( info4.amount.toString() ) );
@@ -129,8 +167,16 @@ async function main(){
         console.log('Scanning blockchain to load all deposit transactions...')
         await scanBlockchain();
     }
-    console.log('Building whitelist based on transaction list...')
-    await generateBalance();
+    if( ! fileExist('accounts_whitelist.txt') ){
+        console.log('Building whitelist based on transaction list...')
+        await generateNewBalance();
+        console.log('Whitelist built!');
+    }
+    if( ! fileExist('accounts_PLTS.csv') ){
+      console.log('Generate formatted file.');
+      await formatWhitelist();
+      console.log('Formatting complete.');
+    }
 }
 
 main();
